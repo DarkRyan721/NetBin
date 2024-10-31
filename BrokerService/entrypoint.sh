9d4a6a2e8e8c0ae568f49e9f629f2027a363666d
DarkRyan721 <MiguelAngelCera@Outlook.com>
2024-10-31 13:54:07 -0500

    Droping new service

--- BrokerService/entrypoint.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo "creating mosquitto password $MQTT_USERNAME $MQTT_PASSWORD"
-mosquitto_passwd -c -b config/passwd $MQTT_USERNAME $MQTT_PASSWORD
-chown -R mosquitto:mosquitto /mosquitto
-/usr/sbin/mosquitto -c config/mosquitto.conf
